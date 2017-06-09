290eb3a9e9d29091eb993a3ff9a7c65c37bbc0e1
Johnny Wei <johntzwei@yahoo.com>
2017-06-09 00:25:35 -0700

    fixed train test error

--- exp_src/experiment.sh
+++ exp_src/experiment.sh
@@ -10,7 +10,7 @@ mkdir $EXP_DIR
 
 #train/test generation
 training_ex=`python $ROOT/trees.py data/$NAME-train generateAllTrees --p1 0 --p2 10 --p3 0 --p4 0`
-testing_ex=`python $ROOT/trees.py data/$NAME-train generateAllTrees --p1 0 --p2 10 --p3 0 --p4 0`
+testing_ex=`python $ROOT/trees.py data/$NAME-test generateAllTrees --p1 0 --p2 10 --p3 0 --p4 0`
 
 #train/test lstm with all architectures
 python $ROOT/lstm.py $training_ex $testing_ex $EXP_DIR/
