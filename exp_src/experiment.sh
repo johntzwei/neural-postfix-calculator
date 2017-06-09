e663dd27797f6a3903319b9fa21f14413a3ecdfb
Johnny Wei <johntzwei@yahoo.com>
2017-06-09 01:03:01 -0700

    fixed more path errors with the experiment bash scripts

--- exp_src/experiment.sh
+++ exp_src/experiment.sh
@@ -13,4 +13,4 @@ training_ex=`python $ROOT/trees.py data/$NAME-train generateAllTrees --p1 0 --p2
 testing_ex=`python $ROOT/trees.py data/$NAME-test generateAllTrees --p1 0 --p2 10 --p3 0 --p4 0`
 
 #train/test lstm with all architectures
-python $ROOT/lstm.py $training_ex $testing_ex $EXP_DIR/
+python $ROOT/lstm.py $ROOT/data/$training_ex $ROOT/data/$testing_ex $EXP_DIR/ --epochs 1000
