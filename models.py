b1d514238ba1783c14221d80fa4077516cd90c29
Johnny Wei <johntzwei@yahoo.com>
2017-06-14 15:26:39 -0700

    seq2seq works runs through but doesnt work

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 import itertools
-
 import numpy as np
+
+import seq2seq
+from seq2seq.models import SimpleSeq2Seq
+
 import keras
 from keras.models import Sequential
 from keras.layers import Masking, Dropout, Activation
@@ -8,15 +11,34 @@ from keras.layers.recurrent import LSTM, SimpleRNN
 
 cmp_all = lambda models, optimizer='adam', loss='mean_squared_error', metrics=['accuracy'] : [ model.compile(optimizer=optimizer, loss=loss, metrics=metrics) for model in models ]
 
+def preliminary_seq2seq(input_shape, output_shape):
+    names = [ 'simple-seq2seq-10' ]
+    nnets = [ seq2seq(input_shape, output_shape, hidden_dims=[10]) ]
+    cmp_all(nnets)
+    return list(zip(names, nnets))
+
+def architectureSearch(input_shape):
+    names = []
+    nnets = []
+    for i in range(5, 25, 5):
+        names.append('2-lstm-%d' % i)
+        nnets.append(lstm(input_shape, hidden_dims=[i]))
+    cmp_all(nnets)
+    return list(zip(names, nnets))
+
 def preliminaries(input_shape):
     names = [ '2-lstm' ]
     nnets = [ lstm(input_shape, hidden_dims=[10]) ]
     cmp_all(nnets)
     return list(zip(names, nnets))
 
+def seq2seq(input_shape, output_shape, hidden_dims=[10], dropout=0.25):
+    model = SimpleSeq2Seq(input_dim=input_shape[1], hidden_dim=hidden_dims[0], output_length=output_shape[0], output_dim=output_shape[1])
+    return model
+
 def lstm(input_shape, hidden_dims=[], dropout=0.25):
     model = Sequential()
-    model.add(Masking(mask_value=np.zeros(input_shape[1]), input_shape=input_shape))
+    model.add(Masking(mask_value=0, input_shape=input_shape))
     for dim in hidden_dims:
         model.add(LSTM(dim, return_sequences=True))
         model.add(Dropout(dropout))
