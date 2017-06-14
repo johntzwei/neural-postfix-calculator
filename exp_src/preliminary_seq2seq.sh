87a7938788e0a3cefa8783701c2dd3811074c274
Johnny Wei <johntzwei@yahoo.com>
2017-06-14 15:35:21 -0700

    preliminary experiment test script

---
+++ exp_src/preliminary_seq2seq.sh
@@ -0,0 +1,40 @@
+# experiment notes
+# tests whether 2 layer lstm with 10 hidden dim can predict
+# 0 depth expressions of arbitrary length, e.g. [133] = 133
+
+ROOT=/home/jwei/neural-postfix-calculator
+NAME=`basename $0 | cut -d . -f 1`
+EXP_DIR=$ROOT/experiments/$NAME
+
+SERIES='preliminary_seq2seq'
+
+EPOCHS=1000
+BATCH_SIZE=32
+TRAIN_TREE_TYPE="generateRandomTrees"
+TRAIN_P1=1
+TRAIN_P2=5
+TRAIN_P3=02
+TRAIN_P4=5
+TRAIN_P5=5000
+
+#the test set may have trees exceeding depth 4
+TEST_TREE_TYPE="generateRandomTrees"
+TEST_P1=1
+TEST_P2=5
+TEST_P3=02
+TEST_P4=5
+TEST_P5=300
+
+mkdir -p $EXP_DIR
+
+#train/test generation
+train_ex="$ROOT/data/$NAME-train_${TRAIN_TREE_TYPE}_${TRAIN_P1}_${TRAIN_P2}_${TRAIN_P3}_${TRAIN_P4}_${TRAIN_P5}"
+test_ex="$ROOT/data/$NAME-test_${TEST_TREE_TYPE}_${TEST_P1}_${TEST_P2}_${TEST_P3}_${TEST_P4}_${TEST_P5}"
+python $ROOT/trees.py $TRAIN_TREE_TYPE --p1 $TRAIN_P1 --p2 $TRAIN_P2 --p3 $TRAIN_P3 --p4 $TRAIN_P4 --p5 $TRAIN_P5 --infix > $train_ex
+python $ROOT/trees.py $TEST_TREE_TYPE --p1 $TEST_P1 --p2 $TEST_P2 --p3 $TEST_P3 --p4 $TEST_P4 --p5 $TEST_P5 --infix > $test_ex
+
+#remove duplicates
+awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 $test_ex f=2 $train_ex > ${train_ex}.dedup
+mv ${train_ex}.dedup $train_ex
+
+python $ROOT/main.py $train_ex $test_ex $SERIES $EXP_DIR --epochs $EPOCHS --batch_size $BATCH_SIZE
