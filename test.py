f543bde412c0aead337f323ec6e947bf5263d411
Johnny Wei <johntzwei@yahoo.com>
2017-06-14 15:32:59 -0700

    fixed testing tools

--- test.py
+++ test.py
@@ -4,27 +4,19 @@ import pickle
 from keras.models import load_model
 
 def predict(expr, model, lb_bin, mem_cells=False):
-    return model.predict(lb_bin.transform(expr))
+    return model.predict(lb_bin.transform(list(expr)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('model', help='the file name of the trained keras model', type=str)
     parser.add_argument('lb_bin', help='the pickled label_binarizer', type=str)
-    parser.add_argument('--testing_ex', help='the tab separated files of testing examples', type=str)
-    parser.add_argument('--mem_cells', help='output the top level lstm memory cells at each stage', type=bool, default=True)
+    parser.add_argument('test_ex', help='the tab separated files of testing examples', type=str)
     args = parser.parse_args()
     
     lb_bin = pickle.load(open(args.lb_bin,'rb'))
     model = load_model(args.model)
     end_char = '$'  #should probably argparse this
     
-    if args.testing_ex != None:
-        print('eval: ', endchar='$')
-        input()
-        while True:
-            expr = str(input())
-            if expr == 'exit':
-                break
-            print(predict(expr, model, lb_bin, mem_cells=args.mem_cells))
-    else:
-        pass
+    test_ex = open(args.test_ex, 'rt')
+    for line in test_ex
+        print('%s\t%s', (line, predict(expr, model, lb_bin)))
